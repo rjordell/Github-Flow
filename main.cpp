bc425d182859c8eb95e0d8e13ade85ef96f41f51
itskristnn <itskristnn@gmail.com>
2022-10-13 13:57:05 -0700

    fixed main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,5 @@
-#include <iostream>
-#include <string>
+#include "c-echo.h"
 
-int main()
-{
-<<<<<<< HEAD
-	    std::string name;
-	        std::cout << "What is your name?" << std::endl;
-		    std::cin >> name;
-		        std::cout << "Hello " << name << "!" << std::endl;
-
-			    return 0;
-=======
-	std::string name;
-	std::cout << "What is your name?" << std::endl;
-	std::cin >> name;
-	std::cout << "Hello " << name << "!" << std::endl;
-
-	return 0;
->>>>>>> 2593264f81ac0076d08ce85693052bf6ff7975f4
+int main(int argv, char** argc) {
+	    std::cout << "Count of output: " << count(echo(argv,argc)) << std::endl;
 }
